7f894de61d0eb4e03253a3d65ba22680d59290cf
Ruslan Ivchenkov <griffit@inbox.ru>
2022-07-12 12:36:54 +0300

    Add project files.

---
+++ Bot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Bot
+{
+    internal class Program
+    {
+        static string token;
+        
+        static TelegramBotClient botClient = new TelegramBotClient(GetToken(token));
+        static string GetToken(string token)
+        {
+            using (StreamReader sr = new StreamReader("token.txt"))
+            {
+               return sr.ReadToEnd();
+            }    
+        }
+    
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello! I am {botClient.GetMeAsync().Result.FirstName}");
+        }
+           
+    }
+}
