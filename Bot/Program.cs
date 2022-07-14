9b315f372faeed93459748fe6b9fc72ef5766ea1
Ruslan Ivchenkov <griffit@inbox.ru>
2022-07-14 10:56:38 +0300

    Функционал приёма сообщений, выдача справки

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 using Telegram.Bot;
-using Telegram.Bot.Args;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Extensions.Polling;
 using Telegram.Bot.Types;
-using Telegram.Bot.Types.Enums;
+
 
 namespace Bot
 {
@@ -19,10 +22,49 @@ namespace Bot
                return sr.ReadToEnd();
             }    
         }
+        public static async Task UpdateAsync (ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                var message = update.Message;
+                if (message.Text.ToLower() == "/start")
+                {
+                    await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать, я бот Хранитель, ознакомиться со списком команд можно набрав /help");
+                    return;
+                }
+                if (message.Text.ToLower() == "/help")
+                {
+                    await botClient.SendTextMessageAsync(message.Chat, "Список команд:" +
+                        "\n/showAll - показать список сохранённых файлов" +
+                        "\n/download {имя файла} - скачать файл");
+                }
+                await botClient.SendTextMessageAsync(message.Chat, "Неизвестная команда");
+            }
+
+        }
+        public static async Task ErrorAsync (ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+        }
     
         static void Main(string[] args)
         {
             Console.WriteLine($"Hello! I am {botClient.GetMeAsync().Result.FirstName}");
+            var cts = new CancellationTokenSource();
+            var cancellationToken = cts.Token;
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { },
+            };
+            botClient.StartReceiving(
+                UpdateAsync,
+                ErrorAsync,
+                receiverOptions,
+                cancellationToken
+                );
+            Console.ReadLine();
+            
+            
         }
            
     }
